6ca11c7450d61915ea9424d76b4155cfefaa508c
Jerome <jerome.baur@students.fhnw.ch>
2024-03-20 21:46:04 +0100

    MultiPage

---
+++ BlazorInheritance.Client/Components/Auftrag.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorInheritance.Client.Components
+{
+    public class Auftrag
+    {
+        public string Name { get; set; }
+        public string Description { get; set; } 
+    }
+}
